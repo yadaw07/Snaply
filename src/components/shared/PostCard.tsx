d355833b2d796f97b8f721fd4bc03f12a997d485
yadaw-hash <tesfaytewelde2015@gmail.com>
2026-06-10 08:50:57 +0300

    added delete post functionality

--- src/components/shared/PostCard.tsx
+++ src/components/shared/PostCard.tsx
@@ -1,11 +1,14 @@
 import type { Post } from '@/types';
 
 import { useAuthStore } from '@/zustand_store/authStore';
-import { Link } from 'react-router';
+import { Link, useNavigate } from 'react-router';
 
 import { formatDate } from '@/lib/utils';
 
 import PostStats from './PostStats';
+import { toast } from 'sonner';
+
+import { useDeletePost } from '@/lib/tanstack-query/queriesAndMutaions';
 
 type PostCardProps = {
   post: Post;
@@ -13,6 +16,25 @@ type PostCardProps = {
 
 const PostCard = ({ post }: PostCardProps) => {
   const user = useAuthStore((state) => state.user);
+
+  const navigate = useNavigate();
+  const { mutate: deletePost } = useDeletePost();
+
+  const handleDeletePost = (e: React.MouseEvent) => {
+    e.preventDefault(); // prevent Link from triggering
+    deletePost(
+      { postId: post.$id, imageId: post.imageId },
+      {
+        onSuccess: () => {
+          toast.success('Post deleted successfully');
+          navigate('/');
+        },
+        onError: () => {
+          toast.error('Failed to delete post. Please try again.');
+        },
+      },
+    );
+  };
 
   return (
     <div className='post-card'>
@@ -43,16 +65,24 @@ const PostCard = ({ post }: PostCardProps) => {
             </div>
           </div>
         </div>
-        <Link
-          to={`update-post/${post.$id}`}
-          className={`${user?.id !== post.creator.$id && 'hidden'}`}
-        >
+        <div className='flex items-center gap-4'>
+          <Link
+            to={`update-post/${post.$id}`}
+            className={`${user?.id !== post.creator.$id && 'hidden'}`}
+          >
+            <img
+              src='assets/icons/edit.svg'
+              alt='edit'
+              className='cursor-pointer w-6 h-6'
+            />
+          </Link>
           <img
-            src='assets/icons/edit.svg'
-            alt='edit'
-            className='cursor-pointer w-6 h-6'
+            src='assets/icons/delete.svg'
+            alt='delete'
+            className={`${user?.id !== post.creator.$id && 'hidden'} cursor-pointer w-6 h-6`}
+            onClick={handleDeletePost}
           />
-        </Link>
+        </div>
       </div>
 
       <Link to={`/posts/${post.$id}`}>
